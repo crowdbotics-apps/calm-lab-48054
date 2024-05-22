d00017ad9924f1eddd44910c3ae880a9bd90cebe
Crowdbotics <team@crowdbotics.com>
2024-05-22 00:23:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-lab-48054",
+  url: "https://calm-lab-48054.botics.co",
+  api: "https://calm-lab-48054.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
